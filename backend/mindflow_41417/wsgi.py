9f05f0b2e03fe7fdf90b804a55f6661e74db5372
Crowdbotics <team@crowdbotics.com>
2023-06-11 16:25:54 +0000

    Initial commit.

---
+++ backend/mindflow_41417/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mindflow_41417 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mindflow_41417.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mindflow-41417",
+  url: "https://mindflow-41417.botics.co",
+  api: "https://mindflow-41417.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
